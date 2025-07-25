c9d513aee9c173786b98d8e0fba59f9a0c5bb1e1
Aidan-Bobs <aidankaroberts@gmail.com>
2025-07-25 13:44:00 +0200

    new repo

---
+++ javascript_for_kids/chapter_6/conditionals_loops.js
@@ -0,0 +1,24 @@
+var lemonChicken = false;
+var beefWithBlackBean = true;
+var sweetAndSourPork = true;
+if (lemonChicken) {
+  console.log("Great! I'm having lemon chicken!");
+} else if (beefWithBlackBean) {
+  console.log("I'm having the beef.");
+} else if (sweetAndSourPork) {
+  console.log("OK, I'll have the pork.");
+} else {
+  console.log("Well, I guess I'll have rice then.");
+}
+
+var sheepCounted = 0;
+while (sheepCounted < 10) {
+  console.log("I have counted " + sheepCounted + " sheep!");
+  sheepCounted++;
+}
+console.log("Zzzzzzzzzzz");
+
+var timesToSayHello = 3;
+for (var i = 0; i < timesToSayHello; i++) {
+  console.log("Hello!");
+}
